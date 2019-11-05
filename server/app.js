8a1d4beed97c83c133fb483a8b158522f3fab586
Mariia Ginzburg <mariia.ginzburg@valtech.de>
2019-11-05 12:00:31 +0100

    implement routing

--- server/app.js
+++ server/app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql'); //name is convention
+const schema = require('./schema/schema');
 
 const app = express();
 
 app.use('/graphql', graphqlHTTP({
-    
+    schema
 })) //routing
 
 app.listen(4000, ()=> {
